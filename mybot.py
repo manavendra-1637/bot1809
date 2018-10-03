a29ad6fc6e19faca2126e77d63e36fd0747865e8
Rulkevich Mikhail <Mrulkevich@fesco.com>
2018-10-03 14:15:42 +1000

    'fix'

--- mybot.py
+++ mybot.py
@@ -19,13 +19,13 @@ def start(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     for item in config.list_buttons:
         keyboard.add(item)
-    bot.send_message(message.chat_id, config.greetings, reply_markup=keyboard)
+    bot.send_message(message.chat.id, config.greetings, reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def answer(message):
     for item in config.list_answers:
         if message.text == item:
-            bot.send_message(message.chat_id, item)
+            bot.send_message(message.chat.id, item)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
